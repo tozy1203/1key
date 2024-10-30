4ff0ddd6457eef65ebdd277fe43f9c68fcc6f7bb
tozy1203 <tozy1203@users.noreply.github.com>
2024-10-30 13:23:18 +0800

    Add files via upload

--- sbox.sh
+++ sbox.sh
@@ -11,12 +11,16 @@ systemctl restart sing-box
 }
 
 main() {
-read -p "输入host: " host
+read -p "输入ws host: " host
+echo "ws host为: $wshost"
 
-echo "host为: $host"
+read -p "输入reality host(回车为host): " rhost
+if [ -z "$rhost" ]; then
+   rhost=$wshost
+fi
+echo "reality host为: $rhost"
 
 read -p "输入httpupgrade path（回车随机）: " path
-
 if [ -z "$path" ]; then
    path=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 5 | head -n 1)
 fi
@@ -24,13 +28,10 @@ echo "path为: $path"
 
 echo "生成uuid"
 uuid=$(sing-box generate uuid)
-output=$(sing-box generate reality-keypair)
+keys=($(sing-box generate reality-keypair))
 
-# 使用awk提取PrivateKey
-prikey=$(echo "$output" | awk '/PrivateKey:/ {print $2}')
-
-# 使用awk提取PublicKey
-pubkey=$(echo "$output" | awk '/PublicKey:/ {print $2}')
+prikey=${keys[1]}
+pubkey=${keys[3]}
 cat > /etc/sing-box/config.json <<EOF
  {
     "inbounds": [
@@ -50,47 +51,47 @@ cat > /etc/sing-box/config.json <<EOF
             }
         },
         {
-            "type": "vless",
+            "type": "shadowsocks",
+            "tag": "ss-in",
             "listen": "127.0.0.1",
-            "listen_port": 8001,
-            "users": [
-                {
-                    "uuid": "$uuid"
-                }
-            ],
-            "transport": {
-                "type": "grpc",
-                "service_name": "$path" 
+            "listen_port": 60080,
+            "method": "none",
+            "password": "",
+            "multiplex": {
+              "enabled": true
             }
         },
         {
-            "type": "vless",
-            "listen": "127.0.0.1",
-            "listen_port": 8002,
-            "users": [
-                {
-                    "uuid": "$uuid"
-                }
-            ],
-            "tls": {
-                "enabled": true,
-                "server_name": "$host",
-                "reality": {
-                    "enabled": true,
-                    "handshake": {
-                        "server": "127.0.0.1",
-                        "server_port": 443
-                    },
-                    "private_key": "$prikey",
-                    "short_id": [
-                        ""
-                    ]
-                }
-            },
-            "transport": {
-                "type": "http",
-                "host": []
+          "type": "vless",
+          "tag": "vless-in",
+          "listen": "127.0.0.1",
+          "listen_port": 60081,
+          "users": [
+            {
+              "uuid": "$uuid",
+              "flow": "xtls-rprx-vision"
             }
+          ],
+          "tls": {
+            "enabled": true,
+            "server_name": "$wshost",
+            "alpn": [
+              "h2",
+              "http/1.1"
+            ],
+            "reality": {
+              "enabled": true,
+              "handshake": {
+                "server": "$wshost",
+                "server_port": 443
+              },
+              "private_key": "$prikey",
+              "short_id": [
+                "",
+                "0123456789abcdef"
+              ]
+            }
+          }
         }
     ],
     "outbounds": [
@@ -102,9 +103,58 @@ cat > /etc/sing-box/config.json <<EOF
 EOF
 cat <<EOF
 套cdn：
-vless://$uuid@ip.sb:80/?type=httpupgrade&encryption=none&host=$host&path=%2F$path%3Fed%3D1024#httpupgrade-$host
-vless://$uuid@127.0.0.1:8001/?type=grpc&encryption=none&serviceName=$path#grpc-$host
-vless://$uuid@127.0.0.1:8002/?type=http&encryption=none&sni=$host&fp=chrome&security=reality&pbk=$pubkey#x-$host
+vless://$uuid@ip.sb:80/?type=httpupgrade&encryption=none&host=$wshost&path=%2F$wshost#httpupgrade-$wshost
+ss://bm9uZTo@@127.0.0.1:60080#ss-$wshost
+vless://$uuid@127.0.0.1:60081/?type=tcp&encryption=none&flow=xtls-rprx-vision&sni=$rhost&fp=chrome&security=reality&pbk=$pubkey#reality-$wshost
+出站json：
+{
+  "type": "vless",
+  "tag": "$wshost",
+  "server": "www.gco.gov.qa",
+  "server_port": 80,
+  "uuid": "$uuid",
+  "transport": {
+    "type": "httpupgrade",
+    "path": "/$path",
+    "Host": "$wshost"
+  }
+},
+{
+  "method": "none",
+  "password": "",
+  "server": "127.0.0.1",
+  "server_port": 60080,
+  "type": "shadowsocks",
+  "multiplex": {
+    "enabled": true,
+    "max_streams": 4,
+    "padding": false,
+    "protocol": "smux"
+  },
+  "tag": "ss"
+},
+{
+  "type": "vless",
+  "server": "127.0.0.1",
+  "server_port": 60081,
+  "uuid": "$uuid",
+  "flow": "xtls-rprx-vision",
+  "tls": {
+    "enabled": true,
+    "insecure": false,
+    "reality": {
+      "enabled": true,
+      "public_key": "$pubkey",
+      "short_id": ""
+    },
+    "server_name": "$rhost",
+    "utls": {
+      "enabled": true,
+      "fingerprint": "chrome"
+    }
+  },
+  "tag": "reality"
+}
 EOF
 
 restart
