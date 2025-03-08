aa8fe68a856b27a4f0def51fc88fb4601f6ed2b0
tozy1203 <tozy1203@users.noreply.github.com>
2025-03-08 11:57:40 +0800

    Add files via upload

--- sbox.sh
+++ sbox.sh
@@ -40,69 +40,119 @@ echo "acme路径为: $acmeRoot"
 echo "生成uuid"
 uuid=$(sing-box generate uuid)
 sspwd=$(sing-box generate rand --base64 16)
+
+echo "生成reality密钥对"
+keypair=$(sing-box generate reality-keypair)
+pkey=$(echo "$keypair" | grep "PrivateKey:" | awk '{print $2}')
+pukey=$(echo "$keypair" | grep "PublicKey:" | awk '{print $2}')
+
+echo "生成short_id"
+sid=$(sing-box generate rand --hex 8)
+
+
 cat > /etc/sing-box/config.json <<EOF
 {
-	"inbounds": [{
-		"type": "vless",
-		"listen": "::",
-		"listen_port": 8080,
-		"users": [{
-			"uuid": "$uuid",
-			"flow": ""
-		}],
-		"transport": {
-			"type": "httpupgrade",
-			"path": "/$path"
-		},
-		"multiplex": {
-			"enabled": true
-			}
-	},
-	{
-		"type": "shadowsocks",
-		"tag": "ss-in",
-		"listen": "127.0.0.1",
-		"listen_port": 5353,
-		"tcp_fast_open": true,
-		"method": "2022-blake3-aes-128-gcm",
-		"password": "$sspwd",
-		"multiplex": {
-			"enabled": true
-		}
-	},
-	{
-		"type": "hysteria2",
-		"tag": "hy2-in",
-		"listen": "::",
-		"listen_port": 443,
-		"users": [{
-			"name": "1",
-			"password": "$uuid"
-		}],
-		"ignore_client_bandwidth": false,
-		"tls": {
-			"enabled": true,
-			"server_name": "$host",
-			"acme": {
-				"domain": ["$host"],
-				"data_directory": "$acmeRoot",
-				"default_server_name": "$host",
-                                "disable_http_challenge": true,
-                                "disable_tls_alpn_challenge": true
-			}
-		},
-		"masquerade": "http://127.0.0.1:5244",
-		"brutal_debug": false
-	}],
-	"outbounds": [{
-		"type": "direct"
-	}]
+    "inbounds": [
+        {
+            "type": "vless",
+            "listen": "::",
+            "listen_port": 8080,
+            "users": [
+                {
+                    "uuid": "$uuid",
+                    "flow": ""
+                }
+            ],
+            "transport": {
+                "type": "httpupgrade",
+                "path": "/$path"
+            },
+            "multiplex": {
+                "enabled": true
+            }
+        },
+        {
+            "type": "vless",
+            "listen": "::",
+            "listen_port": 8443,
+            "users": [
+                {
+                    "uuid": "$uuid",
+                    "flow": "xtls-rprx-vision"
+                }
+            ],
+            "tls": {
+                "enabled": true,
+                "server_name": "$host",
+                "reality": {
+                    "enabled": true,
+                    "handshake": {
+                        "server": "$host",
+                        "server_port": 443
+                    },
+                    "private_key": "$pkey",
+                    "short_id": [
+                        "$sid"
+                    ]
+                }
+            }
+        },
+        {
+            "type": "shadowsocks",
+            "tag": "ss-in",
+            "listen": "127.0.0.1",
+            "listen_port": 5353,
+            "tcp_fast_open": true,
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "$sspwd",
+            "multiplex": {
+                "enabled": true
+            }
+        },
+        {
+            "type": "hysteria2",
+            "tag": "hy2-in",
+            "listen": "::",
+            "listen_port": 443,
+            "users": [
+                {
+                    "name": "1",
+                    "password": "$uuid"
+                }
+            ],
+            "ignore_client_bandwidth": false,
+            "tls": {
+                "enabled": true,
+                "server_name": "$host",
+                "acme": {
+                    "domain": [
+                        "$host"
+                    ],
+                    "data_directory": "$acmeRoot",
+                    "default_server_name": "$host",
+                    "disable_http_challenge": true,
+                    "disable_tls_alpn_challenge": true
+                }
+            },
+            "masquerade": "https://127.0.0.1",
+            "brutal_debug": false
+        }
+    ],
+    "outbounds": [
+        {
+            "type": "direct"
+        }
+    ]
 }
 EOF
 cat > /etc/sing-box/client.outs <<EOF
 套cdn：
 vless://$uuid@ip.sb:80/?type=httpupgrade&encryption=none&host=$cdnHost&path=%2F$path#httpupgrade-$cdnHost
 hy2://$uuid@$host:443/?sni=$host#hy2-$host
+
+Reality直连：
+vless://$uuid@$host:8443/?flow=xtls-rprx-vision&security=reality&sni=$host&fp=chrome&pbk=$pukey&sid=$sid#reality-$host
+
 出站json：
 {
 	"type": "vless",
@@ -145,6 +195,27 @@ hy2://$uuid@$host:443/?sni=$host#hy2-$host
 		"server_name": "$host"
 	},
 	"brutal_debug": false
+},
+{
+	"type": "vless",
+	"tag": "$host.reality",
+	"server": "$host",
+	"server_port": 8443,
+	"uuid": "$uuid",
+	"flow": "xtls-rprx-vision",
+	"tls": {
+		"enabled": true,
+		"server_name": "$host",
+		"utls": {
+			"enabled": true,
+			"fingerprint": "chrome"
+		},
+		"reality": {
+			"enabled": true,
+			"public_key": "$pukey",
+			"short_id": "$sid"
+		}
+	}
 }
 EOF
 viewclient
@@ -152,8 +223,6 @@ restart
 }
 
 menu() {
-
-
 while true; do
     echo "请选择一个选项:"
     echo "1. 新安装"
@@ -180,4 +249,3 @@ while true; do
 done
 }
 menu
-
